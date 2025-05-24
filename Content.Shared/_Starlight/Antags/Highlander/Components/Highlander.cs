28129299f92a928fc0479041e6ab9ade996f8020
BORTBORTBORT <sotetsotetsotetsotetsotet@protonmail.com>
2025-05-24 16:45:43 +0100

    Collective mind and decap sword

---
+++ Content.Shared/_Starlight/Antags/Highlander/Components/Highlander.cs
@@ -0,0 +1,9 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared.Highlander;
+
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class HighlanderComponent : Component
+{
+}
